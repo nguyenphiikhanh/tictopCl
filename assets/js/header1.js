55a5eda85f273f9ff6dd0658380101b6addbd0a9
dev_khanhnp <nguyenphiikhanh@gmail.com>
2022-09-19 23:36:43 +0700

    clone index

---
+++ assets/js/header1.js
@@ -0,0 +1,1 @@
+(function(a,d){if(a._nsl===d){a._nsl=[];var c=function(){if(a.jQuery===d)setTimeout(c,33);else{for(var b=0;b<a._nsl.length;b++)a._nsl[b].call(a,a.jQuery);a._nsl={push:function(b){b.call(a,a.jQuery)}}}};c()}})(window);
